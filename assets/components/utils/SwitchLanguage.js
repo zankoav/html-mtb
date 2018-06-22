50db2b47ec935bf311be80f459a44f16636c83aa
Ctrelka <eduard.ignatyuk.1998@mail.ru>
2018-06-22 13:54:17 +0300

    refactoring code

--- assets/components/utils/SwitchLanguage.js
+++ assets/components/utils/SwitchLanguage.js
@@ -1,10 +1,12 @@
+const LANGUAGE_ATTRIBUTE = '[data-lang]';
+
 export class SwitchLanguage {
     constructor(languages) {
-        const buttonsLangs = document.querySelectorAll('[data-lang]');
-        const languagesString = languageString(languages);
+        const buttonsLangs = document.querySelectorAll(LANGUAGE_ATTRIBUTE);
+        const attributes = getAttributeLanguages(languages);
         for (let i = 0; i < buttonsLangs.length; i++) {
             buttonsLangs[i].addEventListener('click', function () {
-                let elems = document.querySelectorAll(languagesString);
+                let elems = document.querySelectorAll(attributes);
                 let lang = this.dataset.lang;
                 for (let i = 0; i < elems.length; i++) {
                     elems[i].innerHTML = elems[i].dataset[lang];
@@ -14,7 +16,7 @@ export class SwitchLanguage {
     }
 }
 
-function languageString(language) {
+function getAttributeLanguages(language) {
     let resultString = '';
     for (let i = 0; i < language.length; i++) {
         resultString += ('[data-' + language[i] + ']');
